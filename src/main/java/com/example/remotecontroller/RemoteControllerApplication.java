e2fc57e5a1531f53fe3f885b7ae71a51d770c203
kilokokaina <nikolaushki@gmail.com>
2024-09-19 22:23:46 +0800

    Added on start link print"

--- src/main/java/com/example/remotecontroller/RemoteControllerApplication.java
+++ src/main/java/com/example/remotecontroller/RemoteControllerApplication.java
@@ -1,6 +1,7 @@
 package com.example.remotecontroller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.awt.*;
+import java.net.*;
+import java.util.Arrays;
+import java.util.Enumeration;
 
 @Slf4j
 @Controller
@@ -17,6 +21,24 @@ public class RemoteControllerApplication {
 	public static void main(String[] args) {
 		SpringApplicationBuilder builder = new SpringApplicationBuilder(RemoteControllerApplication.class);
 		builder.headless(false).run(args);
+	}
+
+	@Bean
+	public CommandLineRunner cmd() {
+		return args -> {
+			try {
+				var interfaceEnum = NetworkInterface.getNetworkInterfaces();
+				while(interfaceEnum.hasMoreElements()) {
+					for (InterfaceAddress address : interfaceEnum.nextElement().getInterfaceAddresses()) {
+						if (address.getAddress().isSiteLocalAddress()) {
+							log.info("Web-interface is available at: http://" + address.getAddress().getHostAddress() + ":8080");
+						}
+					}
+				}
+			} catch (SocketException e) {
+				log.error(e.getMessage());
+			}
+		};
 	}
 
 	@GetMapping
